2f8d6c99c3f8dd0961cf0d3225d67ad81b93a2fe
Nikolay Gniteev <godhart@gmail.com>
2022-05-26 07:49:24 +0300

    [main] + version info

--- yaml4schm.py
+++ yaml4schm.py
@@ -9,7 +9,10 @@ from yaml4schm_defs import *
 _SKIP_TODO        = True
 _IGNORE_UNCERTAIN = True
 
-_VERSION = "2.0b5.0"
+_VERSION = "2.0b5.1"
+_VERSION_HISTORY = {
+    "2.0": "Refactored v1. Works as standalone app and as a part of server (server.py)",
+}
 _INFO = f"""
 yaml4schm, version {_VERSION}
 
